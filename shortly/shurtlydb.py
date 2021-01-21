ee9142589dfcf7a50ed6b5da9a43abd6a126d1db
Eichhorn <eichhore@at-work.local>
2021-01-21 16:51:13 +0100

    Changed db connection

--- shortly/shurtlydb.py
+++ shortly/shurtlydb.py
@@ -1,12 +1,11 @@
-import psycopg2
-
-dbname = 'shurtly'
-
+import psycopg2, os
+DATABASE_URL = os.environ['DATABASE_URL']
 class Datab():
     def createConnect(self):
-        connect_str = "dbname=" + dbname + " user='shurtlydb' host='localhost' " + \
-                  "password='shurtlyDB__123'"
-        self.con = psycopg2.connect(connect_str)
+        #connect_str = "dbname=" + DATABASE_URL + " user='shurtlydb' host='localhost' " + \
+        #          "password='shurtlyDB__123'"
+        self.con = psycopg2.connect(DATABASE_URL, sslmode='require')
+        #self.con = psycopg2.connect(connect_str)
         self.cursor = self.con.cursor()
     
     def closeConnection(self):
